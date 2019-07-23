ceb5aaeb2932c5c5907dbb3952bf3443145d3b82
hong6234 <hong6234@126.com>
2019-07-23 23:31:36 +0800

    添加logo组件（测试用）

---
+++ components/ssq/logo/logo.js
@@ -0,0 +1,23 @@
+// components/ssq/logo/logo.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
