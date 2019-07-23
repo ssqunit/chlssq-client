f5ee6a6acdf89cd83e2a94205541a592d955759e
hong6234 <hong6234@126.com>
2019-07-23 13:22:14 +0800

    把vant-weapp直接放到components中

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,5 @@
-//index.js
-//获取应用实例
+import Toast from '../../components/vant/toast/toast';
+
 const app = getApp()
 
 // 在需要使用的js文件中，导入js
@@ -8,50 +8,27 @@ var util = require('../../utils/util.js');
 Page({
   data: {
     userInfo: {},
-    time:"",
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-  },
-  // ---------- 事件处理函数 ---------start
-  //跳转-个人详细信息
-  goSelfDetail:function(){
-    wx.navigateTo({
-      url: '../selfDetail/selfDetail'
-    })
-  },
-
-  //跳转商圈搜索页面
-  goSearchPage:function(){
-    wx.navigateTo({
-      url: '../searchSSQ/searchSSQ?type=1'
-    })
+    time: "",
+    searchKeyword: "",
+    postUrls: [
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563866519577&di=7b6bf24aa5b3e7ec54927b3aa829a33a&imgtype=0&src=http%3A%2F%2Fimg.redocn.com%2Fsheji%2F20150127%2Fshejianshangdelvxingmeishihaibaosheji_3895321.jpg",
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563866519577&di=7b6bf24aa5b3e7ec54927b3aa829a33a&imgtype=0&src=http%3A%2F%2Fimg.redocn.com%2Fsheji%2F20150127%2Fshejianshangdelvxingmeishihaibaosheji_3895321.jpg",
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563866519577&di=7b6bf24aa5b3e7ec54927b3aa829a33a&imgtype=0&src=http%3A%2F%2Fimg.redocn.com%2Fsheji%2F20150127%2Fshejianshangdelvxingmeishihaibaosheji_3895321.jpg"
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
   },
 
-  //跳转搜索商圈页面
-  doSearch: function (event) {
-    //TODO 
-  },
-
-  //跳转附近的商圈页面
-  gofjssqPage: function () {
-    wx.navigateTo({
-      url: '../searchSSQ/searchSSQ?type=0'
-    })
-  },
-
-  //跳转创建商圈页面
-  goCreatSSQ: function (event) {
-    wx.navigateTo({
-      url: '../creatSSQ/creatSSQ'
-    })
-  },
-  
-  onLoad: function () {
-
+  onLoad: function(options) {
     var time = util.formatTimeYMD(new Date());
     var weekday = util.getWeekDay(new Date());
+    console.debug(time + " - " + weekday)
     this.setData({
-      time:time +" - "+ weekday
+      time: time + " - " + weekday
     })
 
     if (app.globalData.userInfo) {
@@ -59,7 +36,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -81,6 +58,7 @@ Page({
       })
     }
   },
+
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -88,5 +66,24 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+
+  //跳转搜索商圈页面
+  doSearch: function (e) {
+    //TODO 
+  },
+
+  shopShowMore: function () {
+    Toast({
+      selector: "#van-toast",
+      message: '本圈商家 - 查看更多'
+    });
+  },
+
+  personShowMore: function () {
+    Toast({
+      selector: "#van-toast",
+      message: '本圈个人 - 查看更多'
+    });
+  },
 })
