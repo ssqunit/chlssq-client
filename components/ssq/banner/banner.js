e21d0d9d062ef515f0925317caab71b5f01c58a6
locky <254225634@qq.com>
2019-07-28 11:59:43 +0800

    增加banner广告组件

---
+++ components/ssq/banner/banner.js
@@ -0,0 +1,27 @@
+// components/ssq/banner/banner.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    banners: [
+      { "url": "../../../static/custom/banners/banner_0.png" },
+      { "url": "../../../static/custom/banners/banner_0.png" },
+      { "url": "../../../static/custom/banners/banner_0.png" }
+      ]  
+    },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
