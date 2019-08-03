f9c2a7ad26c3f456b23c6c8ad941cec6ccdb92b0
locky <254225634@qq.com>
2019-08-03 17:04:56 +0800

    我的社商页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,7 +5,36 @@ Page({
    * Page initial data
    */
   data: {
+    pick_name:"御峰园",
+    ssqList:[
+      { "ID": 101, "name": "御峰园", "area": "广东省深圳市龙岗区", "myRole": "成员" },
+      { "ID": 101, "name": "御峰园01", "area": "广东省深圳市龙岗区", "myRole": "成员" },
+      { "ID": 101, "name": "御峰园02", "area": "广东省深圳市龙岗区", "myRole": "成员" },
+      { "ID": 101, "name": "御峰园03", "area": "广东省深圳市龙岗区", "myRole": "成员" }
+    ],
+    mySsqInfo:{
+      name:"双手健康理疗馆",
+      ssqImgUrl:"../../static/custom/defaults/def_ssq.jpg",
+      adCount: 8,
+      productList: [
+        { "ID": "108", "name": "产品名称", "des": "产品的描述信息", "count": 1, "price": 18.00, "tags": [1], "images": ["https://img.yzcdn.cn/vant/t-thirt.jpg"] },
+        { "ID": "108", "name": "产品名称", "des": "产品的描述信息", "count": 1, "price": 18.00, "tags": [1], "images": ["https://img.yzcdn.cn/vant/t-thirt.jpg"] },
+        { "ID": "108", "name": "产品名称", "des": "产品的描述信息", "count": 1, "price": 18.00, "tags": [1], "images": ["https://img.yzcdn.cn/vant/t-thirt.jpg"] },
+        { "ID": "108", "name": "产品名称", "des": "产品的描述信息", "count": 1, "price": 18.00, "tags": [1], "images": ["https://img.yzcdn.cn/vant/t-thirt.jpg"] },
+        { "ID": "108", "name": "产品名称", "des": "产品的描述信息", "count": 1, "price": 18.00, "tags": [1], "images": ["https://img.yzcdn.cn/vant/t-thirt.jpg"] },
+        { "ID": "108", "name": "产品名称", "des": "产品的描述信息", "count": 1, "price": 18.00, "tags": [1], "images": ["https://img.yzcdn.cn/vant/t-thirt.jpg"] },
+        { "ID": "108", "name": "产品名称", "des": "产品的描述信息", "count": 1, "price": 18.00, "tags": [1], "images": ["https://img.yzcdn.cn/vant/t-thirt.jpg"] }
+      ]
+    },
 
+  },
+
+
+  onChange(event) {
+    console.log("tttttttt:"+JSON.stringify(event));
+    this.setData({
+      pick_name: event.detail
+    });
   },
 
   /**
