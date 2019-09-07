d52f814d82fcc254fd8618949f58fc3efb0a6e98
locky <254225634@qq.com>
2019-09-08 00:12:54 +0800

    产品预约

--- pages/my/product/viewer/viewer.js
+++ pages/my/product/viewer/viewer.js
@@ -13,31 +13,15 @@ Page({
   data: {
     showOrderDetail:true,
     orderDetaiLabel:"展开详情",
-    productInfo:{
-      bopInfo:{"ID":198,"name":"双手健康理疗馆","position":{latitude:0.0,longitude:0.0}},
-      "owner":true,//是否是产品拥有者
-      "name":"肩颈理疗",
-      "images": ["../../../../static/icons/map_r.png","../../../../static/custom/defaults/def_ssq.jpg"],
-      "des":"产品描述产品描述产品描述,产品描述产品描述产品描述,产品描述产品描述产品描述,产品描述产品描述产品描述",
-      "price":99.09,
-      "unit":"次",
-      "dateType":1,
-      "dateEnd":15676868900,
-      "tradeType":[1],//交易方式，1为预约
-      "orderInfo":{
-        "status":0, //0未预约，1已预约
-        "detail":{
-          "count":98,
-          "total":998,
-          "list":[
-            {"id":"","name":"jjjjjjjjj","tel":15989316305,"count":3,"date":15764849992},
-            { "id": "", "name": "kkk", "tel": 15989316305, "count": 3, "date": 15764849992 },
-            { "id": "", "name": "cccccc", "tel": 15989316305, "count": 3, "date": 15764849992 }
-          ]
-        }
-      }
+    productInfo:null,
+    tmplist: [
+      { "productid": "", "owner": "", "name": "昵称nickname", "contact": 15989316305, "count": 3, "date": 15764849992, "sum":"8B1501"},
+      { "productid": "", "owner": "", "name": "昵称nickname", "contact": 15989316305, "count": 3, "date": 15764849992, "sum": "8B1501" },
+      { "productid": "", "owner": "", "name": "昵称nickname", "contact": 15989316305, "count": 3, "date": 15764849992, "sum": "8B1501" },
+      { "productid": "", "owner": "", "name": "昵称nickname", "contact": 15989316305, "count": 3, "date": 15764849992, "sum": "8B1501" },
+      { "productid": "", "owner": "", "name": "昵称nickname", "contact": 15989316305, "count": 3, "date": 15764849992, "sum": "8B1501" }
+    ]
 
-    }
 
   },
 
@@ -114,6 +98,12 @@ Page({
             _dateText = util.formatTime(new Date(Number(_info.product.dateend)));
           }
           _info.product['datetext'] = _dateText;
+
+          _info['orderinfo'] = {
+            'count':8,
+            'total':88,
+            'list':that.data.tmplist
+          }
 
           that.setData({
             productInfo: _info
