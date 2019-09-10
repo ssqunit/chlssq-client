8aafc266a15cfb71629eb8db2793056d66ed8604
locky <254225634@qq.com>
2019-09-10 22:02:32 +0800

    社圈新建审核逻辑

--- pages/admin/audit/index.js
+++ pages/admin/audit/index.js
@@ -1,43 +1,44 @@
 // pages/admin/audit/index.js
+
+
+var app = getApp();
+var common = require("../../../utils/common.js")
+var util = require('../../../utils/util.js');
+
+
 Page({
 
   /**
    * Page initial data
    */
   data: {
-    nickName: "locky",
-    count: 8,
-    newSsqList:[
-      { "area": "广东省深圳市龙岗区", "name": "茗萃园" },
-      { "area": "广东省深圳市龙岗区", "name": "茗萃园" },
-      { "area": "广东省深圳市龙岗区", "name": "茗萃园" },
-      { "area": "广东省深圳市龙岗区", "name": "茗萃园" },
-      { "area": "广东省深圳市龙岗区", "name": "茗萃园" },
-      { "area": "广东省深圳市龙岗区", "name": "茗萃园" },
-      { "area": "广东省深圳市龙岗区", "name": "茗萃园" },
-      { "area": "广东省深圳市龙岗区", "name": "茗萃园" }
-    ]
-
+    nickName: "",
+    ssqinfo:null,
+    shopinfo:null,
+    personinfo:null
   },
 
   //
   delSsqApply: function (e) {
+    let id=e.currentTarget.dataset.id;
     wx.navigateTo({
-      url: 'ssqapply/ssqapply',
+      url: 'ssqapply/ssqapply?ssqid='+id,
     })
   },
 
   //
   delBusApply: function (e) {
+    let id = e.currentTarget.dataset.id;
     wx.navigateTo({
-      url: 'busapply/busapply',
+      url: 'busapply/busapply?shopid='+id,
     })
   },
 
   //
   delPerApply: function (e) {
+    let id = e.currentTarget.dataset.id;
     wx.navigateTo({
-      url: 'perapply/perapply',
+      url: 'perapply/perapply?shopid='+id,
     })
   },
 
@@ -52,6 +53,33 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    let that = this;
+    wx.showLoading({ title: '请稍后......' });
+    common.request({
+      method: "GET",
+      url: common.BASE_URL,
+      data: {
+        'function': 'queryAudits',
+        'session_id': app.globalData.userInfo.session_id
+      },
+      success: res => {
+        console.log('-------------queryAudits: res = ' + JSON.stringify(res));
+        wx.hideLoading();
+        if (res.data.iRet == 0) {
+          that.setData({
+            nickName:app.globalData.userInfo.nickName,
+            ssqinfo:res.data.data.ssqinfo,
+            shopinfo:res.data.data.shopinfo,
+            personinfo:res.data.data.personinfo
+          });
+        } else {
+        }
+      },
+      fail: res => {
+        wx.hideLoading();
+        wx.showToast({ title: '请检查网络链接！', icon: "none" })
+      }
+    });
 
   },
 
--- pages/admin/audit/ssqapply/ssqapply.js
+++ pages/admin/audit/ssqapply/ssqapply.js
@@ -1,43 +1,115 @@
 // pages/admin/audit/ssqapply/ssqapply.js
+
+var app = getApp();
+var common = require("../../../../utils/common.js")
+var util = require('../../../../utils/util.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    nickName: "locky",
-    applySsqInfo:{
-      "name":"茗萃园",
-      "area":"广东省深圳市龙岗区",
-      "position":{"latitude":14.989,"longitude":14.989},
-      "image":"../../../../static/custom/defaults/def_ssq.jpg",
-      "creator":{
-        "openid":"",
-        "nickName":"locky"
-      },
-      "nearbySsq":[
-        { "name": "芳园", "area": "广东省深圳市龙岗区", "position": { "latitude": 14.989, "longitude": 14.989 }, "dis": 0.3 },
-        { "name": "芳园", "area": "广东省深圳市龙岗区", "position": { "latitude": 14.989, "longitude": 14.989 }, "dis": 0.3 },
-        { "name": "芳园", "area": "广东省深圳市龙岗区", "position": { "latitude": 14.989, "longitude": 14.989 }, "dis": 0.3 }
-      ]
-    }
-
+    nickName: "",
+    des:"",
+    applySsqInfo:null,
+    nearbySsq:null
   },
 
   //
   showOnMap: function (e) {
     wx.openLocation({
-      latitude: e.currentTarget.dataset.latitude,
-      longitude: e.currentTarget.dataset.longitude,
+      latitude: Number(e.currentTarget.dataset.latitude),
+      longitude: Number(e.currentTarget.dataset.longitude),
       scale: 14
     })
   },
+
+  onDes: function (e) {
+    this.data.des = e.detail.value;
+  },
+
+  onDell: function (e) {
+    let type = e.currentTarget.dataset.type;
+    let _pass = type;
+    wx.showLoading({ title: '请稍后......' });
+    common.request({
+      method: "POST",
+      url: common.BASE_URL + '?function=passSsq&session_id=' + app.globalData.userInfo.session_id,
+      data: {
+        'ssqid': this.data.applySsqInfo.ssqid,
+        'name': this.data.applySsqInfo.name,
+        'owner': this.data.applySsqInfo.owner,
+        'ispass': _pass,
+        'des': this.data.des
+      },
+      success: res => {
+        console.log('-------------passSsq: res = ' + JSON.stringify(res));
+        wx.hideLoading();
+        if (res.data.iRet == 0) {
+          wx.showModal({
+            title: '处理结果',
+            content: '成功！',
+            showCancel: false,
+            success(res) {
+              if (res.confirm) {
+                wx.navigateBack();
+              }
+            }
+          })
+        } else {
+        }
+      },
+      fail: res => {
+        wx.hideLoading();
+        wx.showToast({ title: '请检查网络链接！', icon: "none" })
+      }
+    });
+  },
+
+
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let ssqid = options.ssqid;
+    let that = this;
+    wx.showLoading({ title: '请稍后......' });
+    common.request({
+      method: "GET",
+      url: common.BASE_URL + '?function=querySsqForAudit&session_id=' + app.globalData.userInfo.session_id,
+      data: {
+        'ssqid': ssqid,
+      },
+      success: res => {
+        console.log('-------------querySsqForAudit: res = ' + JSON.stringify(res));
+        wx.hideLoading();
+        if (res.data.iRet == 0) {
+          let _ssqinfo = res.data.data['ssqinfo'];
+          _ssqinfo['img'] = common.getImgUrl(app.globalData.userInfo.session_id, _ssqinfo['imgid']);
+          let _nearbyssq = res.data.data['nearbyssq'];
+          if(_nearbyssq && _nearbyssq.length>0){
+            for(let i=0;i<_nearbyssq.length;i++){
+              let one = _nearbyssq[i];
+              _nearbyssq[i]['dis'] = util.countDistance(_ssqinfo['latitude'], _ssqinfo['longitude'], one['latitude'], one['longitude']);
+            }
+          }
 
+          that.setData({
+            nickName: app.globalData.userInfo.nickName,
+            applySsqInfo: _ssqinfo, 
+            nearbySsq: _nearbyssq           
+          });
+        } else {
+        }
+      },
+      fail: res => {
+        wx.hideLoading();
+        wx.showToast({ title: '请检查网络链接！', icon: "none" })
+      }
+    });
   },
 
   /**
